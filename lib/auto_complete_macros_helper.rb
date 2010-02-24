f70d593c39e6c2e7c9808d6931a6a41e192a5c0b
Marcelo Giorgi <marklazz.uy@gmail.com>
2010-02-24 09:52:03 -0200

    Can set id for a autocomplete field

--- lib/auto_complete_macros_helper.rb
+++ lib/auto_complete_macros_helper.rb
@@ -105,10 +105,11 @@ module AutoCompleteMacrosHelper
   # auto_complete_for to respond the AJAX calls,
   # 
   def text_field_with_auto_complete(object, method, tag_options = {}, completion_options = {})
+    tag_id = tag_options[:id] ? tag_options[:id] : "#{object}_#{method}"
     (completion_options[:skip_style] ? "" : auto_complete_stylesheet) +
     text_field(object, method, tag_options) +
-    content_tag("div", "", :id => "#{object}_#{method}_auto_complete", :class => "auto_complete") +
-    auto_complete_field("#{object}_#{method}", { :url => { :action => "auto_complete_for_#{object}_#{method}" } }.update(completion_options))
+    content_tag("div", "", :id => "#{tag_id}_auto_complete", :class => "auto_complete") +
+    auto_complete_field("#{tag_id}", { :url => { :action => "auto_complete_for_#{object}_#{method}" } }.update(completion_options))
   end
 
   private
