9d2219b52c9e29f3225839c093a4289f10865d43
Marcelo Giorgi <marklazz.uy@gmail.com>
2010-09-27 17:52:14 -0300

    Making text_field_with_auto_complete helper compatible with Rails 3 when used in conjunction with skip_style option

--- lib/auto_complete_macros_helper.rb
+++ lib/auto_complete_macros_helper.rb
@@ -106,7 +106,7 @@ module AutoCompleteMacrosHelper
   # 
   def text_field_with_auto_complete(object, method, tag_options = {}, completion_options = {})
     tag_id = tag_options[:id] ? tag_options[:id] : "#{object}_#{method}"
-    (completion_options[:skip_style] ? "" : auto_complete_stylesheet) +
+    (completion_options[:skip_style] ? "".html_safe : auto_complete_stylesheet) +
     text_field(object, method, tag_options) +
     content_tag("div", "", :id => "#{tag_id}_auto_complete", :class => "auto_complete") +
     auto_complete_field("#{tag_id}", { :url => { :action => "auto_complete_for_#{object}_#{method}" } }.update(completion_options))
