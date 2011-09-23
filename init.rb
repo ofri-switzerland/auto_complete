3b28b966e055ab9f6862545afd777b6c3cfea974
Sebastian Johnsson <sebjoh@axis.com>
2011-09-23 15:37:59 +0200

    Reverting to plugin mode - refactor to gem at a later time...

--- init.rb
+++ init.rb
@@ -1,6 +1,3 @@
-require_relative 'lib/auto_complete'
-require_relative 'lib/auto_complete_macros_helper'
-
 ActionController::Base.send :include, AutoComplete
 ActionController::Base.helper AutoCompleteMacrosHelper
 
